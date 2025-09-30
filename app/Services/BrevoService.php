813022ffd29fc5778e91ff1eb1f58cd04d5ad0c2
Arnaud Bourgoin <arnaud.bourgoin@outlook.com>
2025-10-01 01:03:05 +0200

    Création du service Brevo pour récupérer les campagnes d'emails et les créer

---
+++ app/Services/BrevoService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use Brevo\Client\Api\EmailCampaignsApi;
+use Brevo\Client\Configuration;
+use Brevo\Client\Model\CreateEmailCampaign;
+use Exception;
+use GuzzleHttp\Client;
+
+class BrevoService
+{
+    private $apiInstance;
+
+    public function __construct()
+    {
+        // Initialisation du client Brevo avec la clé API
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', env('BREVO_API_KEY'));
+
+        $this->apiInstance = new EmailCampaignsApi(new Client(), $config);
+    }
+
+    /**
+     * Crée une campagne email via l'API Brevo
+     * @param array $emailCampaignData
+     * @param string $senderEmail
+     * @param string $senderName
+     * @return mixed|null
+     */
+    public function createEmailCampaign($emailCampaignData, $senderEmail = "aymane.allinbox+brevo+sde@gmail.com", $senderName = "AllInBox")
+    {
+
+        $createCampaign = new CreateEmailCampaign();
+        $createCampaign->setSender(['name' => $senderName, 'email' => $senderEmail]);
+        $createCampaign->setSubject($emailCampaignData['subject']);
+        $createCampaign->setHtmlContent($emailCampaignData['content']);
+        $createCampaign->setName($emailCampaignData['title']);
+
+        try {
+            $result = $this->apiInstance->createEmailCampaign($createCampaign);
+            return $result;
+        } catch (Exception $e) {
+            echo 'Exception when calling EmailCampaignsApi->createEmailCampaign: ', $e->getMessage(), PHP_EOL;
+            return null;
+        }
+    }
+
+    /**
+     * Récupère toutes les campagnes email via l'API Brevo
+     * @return mixed|null
+     */
+    public function getEmailCampaigns()
+    {
+        try {
+            $result = $this->apiInstance->getEmailCampaigns();
+            return $result;
+        } catch (Exception $e) {
+            echo 'Exception when calling EmailCampaignsApi->getEmailCampaigns: ', $e->getMessage(), PHP_EOL;
+            return null;
+        }
+    }
+}
--- resources/views/email-campaign/create.blade.php
+++ resources/views/email-campaign/create.blade.php
@@ -1,15 +1,4 @@
-<!-- resources/views/campaigns/create.blade.php -->
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
-</head>
-
-<body>
+<x-layout>
     <x-bladewind::notification />
 
     @if (session('success'))
@@ -20,26 +9,43 @@
             });
         </script>
     @endif
-
-    <div class="container mx-auto mt-6">
-
-        <h1 class="text-center text-2xl font-bold">Créer une nouvelle campagne email</h1>
-        <x-bladewind::card title="Formulaire">
-            <form method="POST" action="{{ route('email-campaign.store') }}">
-                @csrf
-                <div class="flex gap-1">
-                    <x-bladewind::input required="true" label="Titre" name="title" value="{{ old('title') }}" />
-                    <x-bladewind::input required="true" label="Objet" name="subject" value="{{ old('subject') }}" />
+    @if (session('error'))
+        <script>
+            // Initialisation de la notification après chargement de la page
+            window.addEventListener('load', function() {
+                showNotification('{{ session('error') }}', 'Erreur de création', 'error', 3);
+            });
+        </script>
+    @endif
+    <h1 class="text-center text-2xl font-bold mb-4">Créer une nouvelle campagne email</h1>
+    <x-bladewind::card title="Formulaire">
+        <form method="POST" action="{{ route('email-campaign.store') }}">
+            @csrf
+            <div class="flex gap-1">
+                <div class="flex-1 flex-col">
+                    <x-bladewind::input required="true" label="Titre" name="title" error_message="Vous devez entrer le titre" value="{{ old('title') }}" />
+                    @error('title')
+                        <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                    @enderror
                 </div>
+                <div class="flex-1 flex-col">
+                    <x-bladewind::input required="true" label="Objet" name="subject" error_message="Vous devez entrer l'objet" value="{{ old('subject') }}" />
+                    @error('subject')
+                        <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                    @enderror
+                </div> 
+            </div>
 
-                <x-bladewind::textarea required="true" label="Contenu" name="content" value="{{ old('content') }}" />
+            <div class="flex flex-col">
+                <x-bladewind::textarea required="true" label="Contenu" name="content" error_message="Vous devez entrer le contenu" value="{{ old('content') }}" />
+                @error('content')
+                    <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                @enderror
+            </div>
+            
 
-                <x-bladewind::button can_submit="true">Créer la campagne</x-bladewind::button>
-            </form>
-        </x-bladewind::card>
+            <x-bladewind::button can_submit="true" has_spinner="true" icon="paper-airplane">Créer la campagne</x-bladewind::button>
+        </form>
+    </x-bladewind::card>
 
-    </div>
-    <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
-</body>
-
-</html>
+</x-layout>
